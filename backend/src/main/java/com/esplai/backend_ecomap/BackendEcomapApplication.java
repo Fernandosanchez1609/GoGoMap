3fcc807eb825dbfa82d49283873118cb1e077e49
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-04-28 22:23:01 +0200

    chore: initialize monorepo structure with Spring Boot and React+TS

---
+++ backend/src/main/java/com/esplai/backend_ecomap/BackendEcomapApplication.java
@@ -0,0 +1,13 @@
+package com.esplai.backend_ecomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEcomapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEcomapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/esplai/backend_ecomap/BackendEcomapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esplai.backend_ecomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEcomapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
