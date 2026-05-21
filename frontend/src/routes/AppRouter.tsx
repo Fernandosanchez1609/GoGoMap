aa2ed116023c28ea7fd99c84c7a4fca62aadaf74
naygharbi <naylagharbi@outlook.es>
2026-05-21 11:38:39 +0200

    Feat: Maquetado del  Login page

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import MapPage from "@/pages/Map/MapPage";
 import GoGomapOnboarding from "@/components/onboarding/GoGomapOnboarding";
+import Login from "@/components/Login/Login";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<GoGomapOnboarding />} />
       <Route path="/map" element={<MapPage />} />
+      <Route path="/login" element={<Login/>} />
     </Routes>
   )
 }
