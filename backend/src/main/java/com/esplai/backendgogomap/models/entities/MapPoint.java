8eb07e9bf63f1987ae9e7f9e7c387fe8f9cf53ed
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-05-21 09:56:41 +0200

    feat(backend): implement MapPoint entity with temporary foreign keys and fix package name to match folder structure

---
+++ backend/src/main/java/com/esplai/backendgogomap/models/entities/MapPoint.java
@@ -0,0 +1,53 @@
+package com.esplai.backendgogomap.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "map_points")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MapPoint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 300)
+    private String description;
+
+    private String address;
+
+
+    @Column(nullable = false)
+    private Double latitude;
+
+    @Column(nullable = false)
+    private Double longitude;
+
+
+    private String status;
+
+    @CreationTimestamp
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name="last_verified_at")
+    private LocalDateTime lastVerifiedAt;
+
+    @Column(nullable = false,name="ods_id")
+    private Long odsId;
+
+    @Column(name="reported_by")
+    private Long reportedBy;
+
+}
