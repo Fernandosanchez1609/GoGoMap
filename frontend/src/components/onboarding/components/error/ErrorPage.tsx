672fa93b9e5ed9afa377ef7cfa92737ccc9268b6
Joaquín Domínguez <jdominguezsantana@gmail.com>
2026-05-23 14:33:48 +0200

    Implementado ErrorImage.tsx

--- frontend/src/components/onboarding/components/error/ErrorPage.tsx
+++ frontend/src/components/onboarding/components/error/ErrorPage.tsx
@@ -1,9 +1,11 @@
 import ErrorHeader from "./ErrorHeader"
+import ErrorImage from "./ErrorImage"
 
 export default function ErrorPage() {
     return (
         <div className="min-h-screen bg-[#F5F5EE] flex flex-col items-center px-6">
             <ErrorHeader />
+            <ErrorImage />
             {/* Aquí irá el Contenido principal */}
             {/* Aquí irá el Footer */}
         </div>
