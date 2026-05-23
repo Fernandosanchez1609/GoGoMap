058ef198861a5874677629987532689ade3c76de
Joaquín Domínguez <jdominguezsantana@gmail.com>
2026-05-23 14:42:07 +0200

    Implementado ErrorText.tsx

--- frontend/src/components/onboarding/components/error/ErrorPage.tsx
+++ frontend/src/components/onboarding/components/error/ErrorPage.tsx
@@ -1,11 +1,13 @@
 import ErrorHeader from "./ErrorHeader"
 import ErrorImage from "./ErrorImage"
+import ErrorText from "./ErrorText"
 
 export default function ErrorPage() {
     return (
         <div className="min-h-screen bg-[#F5F5EE] flex flex-col items-center px-6">
             <ErrorHeader />
             <ErrorImage />
+            <ErrorText />
             {/* Aquí irá el Contenido principal */}
             {/* Aquí irá el Footer */}
         </div>
