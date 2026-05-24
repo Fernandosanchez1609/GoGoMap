357eb0ed5a4614ed0b9007a5b96bb1e777c0b98e
Joaquín Domínguez <jdominguezsantana@gmail.com>
2026-05-24 09:18:59 +0200

    Creación de la raiz de Favoritos

---
+++ frontend/src/pages/Favorites/FavoritesPage.tsx
@@ -0,0 +1,17 @@
+// Estilos
+const page = "min-h-screen bg-[#F5F5EE] flex flex-col max-w-md mx-auto relative"
+const content = "flex-1 flex flex-col pb-24"
+
+// Componente
+export default function FavoritesPage() {
+    return (
+        <div className={page}>
+            <div className={content}>
+                {/* Aquí irá: FavoritesHeader */}
+                {/* Aquí irá: FavoritesFilter */}
+                {/* Aquí irá: FavoritesList */}
+            </div>
+            {/* Aquí irá: BottomNav */}
+        </div>
+    )
+}
--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -3,6 +3,9 @@ import MapPage from "@/pages/Map/MapPage";
 import GoGomapOnboarding from "@/components/onboarding/GoGomapOnboarding";
 import Login from "@/components/Login/Login";
 import Register from "@/components/Register/Register";
+import ErrorPage from "@/components/error/ErrorPage";
+import ErrorPage404 from "@/components/error404/ErrorPage404";
+import FavoritesPage from "@/pages/Favorites/FavoritesPage";
 
 export default function AppRouter() {
   return (
@@ -11,6 +14,9 @@ export default function AppRouter() {
       <Route path="/map" element={<MapPage />} />
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<Register/>} />
+      <Route path="/errorpage" element={<ErrorPage/>} />
+      <Route path="/errorpage404" element={<ErrorPage404/>} />
+      <Route path="/favoritespage" element={<FavoritesPage/>} />
 
     </Routes>
   )
