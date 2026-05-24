5d2f55cc75358afd4f659a43bf0ef41bb6edc4a2
zitor83 <ortizsanchezjoseantonio@gmail.com>
2026-05-24 16:44:56 +0200

    feat(auth): add DTOs and Role enum for authentication module

---
+++ backend/src/main/java/com/esplai/backendgogomap/auth/authDto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.esplai.backendgogomap.auth.authDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String tokenType = "Bearer";
+
+    // TODO UsuarioResponseDTO cuando este creado
+}
