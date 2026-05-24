722dc981473aa8cb85df75793bf80e5a872247b2
Joaquín Domínguez <jdominguezsantana@gmail.com>
2026-05-24 19:06:43 +0200

    Favoritos implementada

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+
 export default function Footer() {
   return (
     <footer className="display flex   justify-around p-4 bg-white border-t">
--- frontend/src/pages/Favorites/FavoritesPage.tsx
+++ frontend/src/pages/Favorites/FavoritesPage.tsx
@@ -1,17 +1,39 @@
+import FavoritesHeader from "./FavoritesHeader"
+import FavoritesTitle from "./FavoritesTitle"
+import { useState } from "react"
+import Filter from "@/components/Map/Filter"
+import FavoritesList from "./FavoritesList"
+import Footer from "@/components/Footer/Footer"
+
 // Estilos
 const page = "min-h-screen bg-[#F5F5EE] flex flex-col items-center"
 const content = "w-full flex-1 flex flex-col pb-24"
 
+const filterWrapper = "w-full overflow-hidden"
+const filterOffset  = "-mt-[84px]"
+
+const footerWrapper = "fixed bottom-0 left-0 w-full z-50"
+
 // Componente
 export default function FavoritesPage() {
+    const [selectedOds, setSelectedOds] = useState<number | null>(null)
+
     return (
         <div className={page}>
             <div className={content}>
-                {/* Aquí irá: FavoritesHeader */}
-                {/* Aquí irá: FavoritesFilter */}
-                {/* Aquí irá: FavoritesList */}
+                <FavoritesHeader />                
+                <div className={filterWrapper}>
+                    <div className={filterOffset}>
+                        <Filter selected={selectedOds} onSelect={setSelectedOds} />
+                    </div>
+                </div>
+                <FavoritesTitle />
+                <FavoritesList selectedOds={selectedOds} />
             </div>
-            {/* Aquí irá: BottomNav */}
+            <div className={footerWrapper}>
+                <Footer />
+            </div>
         </div>
     )
+
 }
