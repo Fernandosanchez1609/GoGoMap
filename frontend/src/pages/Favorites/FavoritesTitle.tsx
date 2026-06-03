8af1e7b925ca756c0a3e07616c2f31ddf05060b8
Joaquín Domínguez <jdominguezsantana@gmail.com>
2026-06-03 18:42:47 +0200

    feat: increase Favoritos title size

--- frontend/src/pages/Favorites/FavoritesTitle.tsx
+++ frontend/src/pages/Favorites/FavoritesTitle.tsx
@@ -1,6 +1,6 @@
 // Estilos
 const wrapper = "bg-app-surface-1 px-4 pb-4 w-full"
-const title = "text-2x1 font-bold text-green-700"
+const title = "text-2xl font-bold text-green-700"
 
 // Componente
 export default function  FavoritesTitle() {
